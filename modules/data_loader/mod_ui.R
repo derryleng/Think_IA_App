3e0a7488ba5f3ebca7779857f14adc7cac564999
derry-dev <47482284+derry-dev@users.noreply.github.com>
2020-10-23 20:36:13 +0100

    23/10/2020

--- modules/data_loader/mod_ui.R
+++ modules/data_loader/mod_ui.R
@@ -4,7 +4,53 @@ data_loader_ui <- function(id) {
   
   div(
     
+    class = "wrapper",
     
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "flex.css")
+    ),
+    
+    box(
+      class = "mid_1",
+      title = "Database Connection",
+      width = NULL,
+      collapsible = F
+    ),
+    
+    box(
+      class = "mid_2",
+      title = "Latest Command",
+      width = NULL,
+      collapsible  = F
+    ),
+    
+    box(
+      class = "left_1",
+      title = "Data Loading",
+      width = NULL,
+      collapsible  = F
+    ),
+    
+    box(
+      class = "right_1",
+      title = "Generic Processing",
+      width = NULL,
+      collapsible  = F
+    ),
+    
+    box(
+      class = "right_2",
+      title = "TBS Specific Processing",
+      width = NULL,
+      collapsible  = F
+    ),
+    
+    box(
+      class = "right_3",
+      title = "Adaptation Management",
+      width = NULL,
+      collapsible  = F
+    ),
     
   )
   
